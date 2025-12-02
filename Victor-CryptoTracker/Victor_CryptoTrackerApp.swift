bde15129e6e0b06f23e3359a7f95fccc06fbe7ac
Victor Vilo <githinjivictor9@gmail.com>
2025-12-02 15:21:30 +0300

    Initial Commit

---
+++ Victor-CryptoTracker/Victor_CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Victor_CryptoTrackerApp.swift
+//  Victor-CryptoTracker
+//
+//  Created by Victor on 02/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Victor_CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
